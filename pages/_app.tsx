c49cd2ec203491b00a6033fc2a56be39a98bd888
Crude Finance <bitsharescrude@gmail.com>
2023-11-25 16:12:47 +0100

    change

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import Navbar from "../components/Navbar";
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
 // You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "arbitrum";
+const activeChain = "binance";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -15,20 +15,20 @@ function MyApp({ Component, pageProps }: AppProps) {
       activeChain={{// === Required information for connecting to the network === \\
         chainId: 42161, // Chain ID of the network
         // Array of RPC URLs to use
-        rpc: ["https://rpc.arb1.arbitrum.gateway.fm"],
+        rpc: ["https://bsc-dataseed.bnbchain.org"],
 
         // === Information for adding the network to your wallet (how it will appear for first time users) === \\
         // Information about the chain's native currency (i.e. the currency that is used to pay for gas)
         nativeCurrency: {
           decimals: 18,
-          name: "Arbitrum One",
-          symbol: "ARB",
+          name: "BNB Smart Chain Mainnet",
+          symbol: "BNB",
         },
-        shortName: "arbitrum", // Display value shown in the wallet UI
-        slug: "arbitrum", // Display value shown in the wallet UI
+        shortName: "binance", // Display value shown in the wallet UI
+        slug: "binance", // Display value shown in the wallet UI
         testnet: false, // Boolean indicating whether the chain is a testnet or mainnet
-        chain: "arbitrum", // Name of the network
-        name: "Arbitrum One", // Name of the network
+        chain: "binance", // Name of the network
+        name: "BNB Smart Chain Mainnet", // Name of the network
 }}
     ><ChakraProvider>
       <Navbar />
